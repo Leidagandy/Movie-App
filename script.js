911d112529774742cc38b23219ebed11ac74692c
Leida_Gandy <leida.gandy@gmail.com>
2021-02-16 09:14:52 +0000

    Add fetch and image error handling

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH =
   'https://api.themoviedb.org/3/search/movie?api_key=6695aa583ebb10464b797d366f458a2f&query="';
 
-const logo = document.getElementsByClassName("logo");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const main = document.getElementById("main");
+const imageNotAvailable =
+  "https://sainfoinc.com/wp-content/uploads/2018/02/image-not-available.jpg";
 
 const fetchMovies = async (url) => {
-  const response = await fetch(url);
-  const movies = await response.json();
-  showMovies(movies.results);
+  try {
+    const response = await fetch(url);
+    const movies = await response.json();
+    showMovies(movies.results);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchMovies(API_URL);
@@ -29,25 +34,22 @@ function showMovies(movies) {
 
     movieNode.innerHTML = `
 <img
-  src="${IMG_PATH + poster_path}"
+  src="${poster_path ? IMG_PATH + poster_path : imageNotAvailable}"
   alt="${title}"
 />
 <div class="movie-info">
   <h3>${title}</h3>
   <span class="rating">${vote_average}</span>
 </div>
+
 <div class="overview">
   <h3>Overview</h3>
- ${overview}
+ ${overview ? overview : "No overview available"}
 </div>
 `;
     main.appendChild(movieNode);
   });
 }
-
-// logo.addEventListener("click", () => {
-//   return window.location.reload();
-// });
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
